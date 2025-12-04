4141e094d5b9d4898c1640e4044d1d181398f164
yasemitee <116098124+yasemitee@users.noreply.github.com>
2025-12-04 11:44:01 +0100

    feat: add Simple Analytics script for tracking user interactions

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,11 @@ export default function RootLayout({
           content="black-translucent"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/*Simple Analytics*/}
+        <script
+          async
+          src="https://scripts.simpleanalyticscdn.com/latest.js"
+        ></script>
       </head>
       <body
         className={lexendExa.className}
